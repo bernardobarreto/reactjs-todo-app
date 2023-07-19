89db5f7f0b72baea82397b4fb41294b40564dc80
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-07-19 19:46:37 -0300

    create add to list feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import SubmitForm from './components/submit_form'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [list, setList] = useState([]);
+
   return (
     <div className="App">
-      <SubmitForm />
+      <SubmitForm list={list} setList={setList} />
+      <ul>
+      { list.map((item, index) => (
+        <li key={index}>{item}</li>
+      )) }
+    </ul>
     </div>
   );
 }
--- src/components/submit_form.js
+++ src/components/submit_form.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SubmitForm extends React.Component {
-  render() {
-    return (
-      <form>
-        <input placeholder='Task name'></input>
-        <button type='submit'>Add</button>
-      </form>
-    )
+function SubmitForm(props) {
+  const [taskName, setTaskName] = useState('');
+
+  const saveTask = (e) => {
+    e.preventDefault()
+    props.setList(props.list.concat(taskName))
+    setTaskName('')
   }
+
+  return (
+    <form onSubmit={saveTask}>
+      <input placeholder='Task name' value={taskName} onChange={(e) => setTaskName(e.target.value)}></input>
+      <button type='submit'>Add</button>
+    </form>
+  )
 }
 
 export default SubmitForm;
