daec80c8c5217c0d3addd571e91a3ce5877552b0
Bernardo B. Marques <bernardobarreto.dev@gmail.com>
2023-07-22 16:24:10 -0300

    store todo list at LS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import SubmitForm from './components/submit_form';
 import List from './components/list';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [list, setList] = useState([]);
+  const keyOnLS = 'todoList';
+  const getListFromLS = () => {
+    return JSON.parse(localStorage.getItem(keyOnLS)) || [];
+  }
+  const setListToLS = () => {
+    localStorage.setItem(keyOnLS, JSON.stringify(list));
+  }
+
+  const [list, setList] = useState(getListFromLS);
+
+  useEffect(setListToLS);
 
   return (
     <div className="App">
